bb3b51a10604fedf02d52ddd4a4c6f6508c2e687
Drellimal2 <dmiller2310@hotmail.com>
2015-09-17 11:47:25 -0500

    Added more functionality to Reversi/Othello

--- reversi_othello.py
+++ reversi_othello.py
@@ -9,7 +9,7 @@ from os import system
 BLACK = '\033[0;40m'
 WHITE = '\033[1;47m'
 T_WHITE = '\033[1;37m'
-BLUE = '\033[1;44m'
+BLUE = '\033[1;46m'
 GREEN = '\033[1;42m'
 ENDC = '\033[0m'
 
@@ -39,6 +39,7 @@ change_color(36,1)
 
 
 def check_hori(pos, col, s, e):
+    result = 0
     left  = []
     right = []
     l_pos = 0
@@ -57,6 +58,7 @@ def check_hori(pos, col, s, e):
                 change_color(pos, col)
                 for y in left:
                     change_color(y, col)
+                result += len(left)
             break
         
         elif int(board[l_pos][-7:-4]) == op_col:
@@ -74,6 +76,7 @@ def check_hori(pos, col, s, e):
                 change_color(pos, col)
                 for y in right:
                     change_color(y, col)
+                result += len(right)
             break
         
         elif int(board[r_pos][-7:-4]) == op_col:
@@ -81,9 +84,122 @@ def check_hori(pos, col, s, e):
             r_pos +=1
         else:
             break
-    return 5
-    
+    return result
+
+def get_player(col):
+    if col == 1:
+        return WHITE + T_WHITE +" 1 " +ENDC
+    else:
+        return BLACK + " 0 " + ENDC
+
+def check_diag(pos, col):
+    result = 0
+    t_edge = [x for x in range(8)]
+    b_edge = [x for x in range(56,64)] 
+    l_edge = [x for x in range(8,49,8)]
+    r_edge = [x for x in range(15,56,8)]
+    changed = []
+    op_col = (col+1)%2
+
+    npos = 0
+    npos += pos
+    npos -= 9
+    if pos not in (t_edge +l_edge):
+        while npos not in (t_edge + l_edge):
+            if board[npos][-7] == "0":
+                break
+            elif int(board[npos][-7:-4]) == col:
+                if changed == []:
+                    break
+                else:
+                    change_color(pos, col)
+                    for y in changed:
+                        change_color(y, col)
+                    result += len(changed)
+                    changed = []
+
+                break
+            
+            elif int(board[npos][-7:-4]) == op_col:
+                changed += [npos]
+                npos -=9
+            else:
+                break
+    npos = 0
+    npos += pos
+    npos += 9
+    if pos not in (b_edge +r_edge):
+        while npos not in (b_edge + r_edge):
+            if board[npos][-7] == "0":
+                break
+            elif int(board[npos][-7:-4]) == col:
+                if changed == []:
+                    break
+                else:
+                    change_color(pos, col)
+                    for y in changed:
+                        change_color(y, col)
+                    result += len(changed)
+                    changed = []
+                break
+            
+            elif int(board[npos][-7:-4]) == op_col:
+                changed += [npos]
+                npos +=9
+            else:
+                break
+    npos = 0
+    npos += pos
+    npos -= 7
+    if pos not in (t_edge +r_edge):
+        while npos not in (t_edge + r_edge):
+            if board[npos][-7] == "0":
+                break
+            elif int(board[npos][-7:-4]) == col:
+                if changed == []:
+                    break
+                else:
+                    change_color(pos, col)
+                    for y in changed:
+                        change_color(y, col)
+                    result += len(changed)
+                    changed = []
+
+                break
+            
+            elif int(board[npos][-7:-4]) == op_col:
+                changed += [npos]
+                npos -=7
+            else:
+                break
+    npos = 0
+    npos += pos
+    npos += 7
+    if pos not in (b_edge +l_edge):
+        while npos not in (b_edge + l_edge):
+            if board[npos][-7] == "0":
+                break
+            elif int(board[npos][-7:-4]) == col:
+                if changed == []:
+                    break
+                else:
+                    change_color(pos, col)
+                    for y in changed:
+                        change_color(y, col)
+                    result += len(changed)
+                    changed = []
+
+                break
+            
+            elif int(board[npos][-7:-4]) == op_col:
+                changed += [npos]
+                npos +=7
+            else:
+                break
+    return result
+            
 def check_verti(pos, col, s, e):
+    result = 0
     top  = []
     bot = []
     t_pos = 0
@@ -102,6 +218,7 @@ def check_verti(pos, col, s, e):
                 change_color(pos, col)
                 for y in top:
                     change_color(y, col)
+                result += len(top)
             break
         
         elif int(board[t_pos][-7:-4]) == op_col:
@@ -120,6 +237,7 @@ def check_verti(pos, col, s, e):
                 change_color(pos, col)
                 for y in bot:
                     change_color(y, col)
+                result += len(bot)
             break
         
         if int(board[b_pos][-7:-4]) == op_col:
@@ -127,12 +245,14 @@ def check_verti(pos, col, s, e):
             b_pos +=8
         else:
             break
-    return 5
+    return result
             
 def play(pos, player):
-    check_hori(pos, player,pos- (pos%8),  pos- (pos%8)+8)
-    check_verti(pos, player, pos%8, 56 + pos%8)
+    a = check_hori(pos, player,pos- (pos%8),  pos- (pos%8)+8)
+    b = check_verti(pos, player, pos%8, 56 + pos%8)
+    c = check_diag(pos, player)
     
+    return a+b+c 
 
     
 nums_board = [add_padd(x) for x in range(64)]
@@ -151,13 +271,32 @@ def print_board(board):
         print   (BLUE+"|"+ENDC).join(board2) 
         print   (BLUE+"|"+ENDC).join(board4)
 
+players = [2,2]
+
 def game(board):
     system('cls')
     print_board(board)
     player = 0
     for x in range(60):
-        y = raw_input("Player " + str(player+1) +"select tile:")
-        play(int(y), player)
+        while(1):
+            print players
+            y = raw_input("|" +get_player(player) +"| select tile:")
+            z = play(int(y), player)
+            print z
+            if z > 0:
+                players[player] += z +1
+                players[(player+1)%2] -= z
+                break
+            else:
+                system('cls')
+                print_board(board)
+                print "Select Valid Position"
+        
         player = (player+1)%2
+        if players[player] == 0:
+            player = (player+1)%2
+            print "|" +get_player(player) +"| wins" 
         system('cls')
+        
         print_board(board)
+        print z
